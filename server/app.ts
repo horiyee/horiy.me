a5e908bce44bd4d202135b75841ca99b0cc1f46a
horiyee <kaito@horiyee.dev>
2024-08-10 23:09:58 +0900

    feat: add legacy bookmarks api

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,32 @@ import { Hono } from "./deps.ts";
 
 const app = new Hono();
 
-app.get("/api", (c) => c.json({ message: "Hello!" }));
+app.get("/legacy/bookmarks", async (c) => {
+  const kv = await Deno.openKv();
+
+  await kv
+    .get(["legacy", "bookmarks"])
+    .then((result) => c.json({ bookmarks: result.value }))
+    .catch(console.error);
+});
+
+app.post("/legacy/bookmarks", async (c) => {
+  const json: {
+    bookmarks: {
+      url: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+  } = await c.req.json();
+
+  const { bookmarks } = json;
+
+  const kv = await Deno.openKv();
+
+  await kv.set(["legacy", "bookmarks"], bookmarks).then(() => {
+    c.status(201);
+    c.json({ bookmarks });
+  });
+});
 
 export default app;
