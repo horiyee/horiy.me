d27758e8a5677e9e61c309de82a77b60881caba4
horiyee <kaito@horiyee.dev>
2024-08-12 14:15:53 +0900

    chore: delete health check api

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,7 @@
 import { Application } from "./deps.ts";
-import healthCheck from "./routes/healthCheck.ts";
 import blogPost from "./routes/blogPost.ts";
 
 const app = new Application();
-
-app.use(healthCheck.routes());
 
 app.use(blogPost.routes());
 
--- server/routes/healthCheck.ts
+++
@@ -1,9 +0,0 @@
-import { HealthCheckController } from "../controllers/healthCheck.ts";
-import { Router } from "../deps.ts";
-
-const router = new Router();
-const healthCheckController = HealthCheckController();
-
-router.get("/health_check", healthCheckController.index);
-
-export default router;
